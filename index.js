786c705f0b2f0546cc73d2bbf4247bec3513b293
Scholastica Nzomo <scholambula1@gmail.com>
2023-04-01 09:02:26 +0300

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = (["Milo", "Otis", "Garfield"]);
+
+function logCats(cats) {
+  console.log("cats:", cats);
+}
+
+logcats(cats);
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph"); 
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name) {
+    let newCatsArray = [...cats, "Broom"];
+    return newCatsArray;
+  }let newCatsArray = appendCat();
+
+  function prependCat(name) {
+    let newCatsArray = ["Arnold", ...cats];
+    return newCatsArray;
+  }let newCatsArray2 = appendCat();
+
+  function removeLastCat() {
+    let copyOfCats=cats.slice(0,2)
+return copyOfCats;
+  }
+  let copyOfCats2=removeLastCat();
+
+  function removeFirstCat(){
+   let copyOfCats3=cats.slice(1)
+    return copyOfCats3;
+  }
+  let copyOfCats4=removeFirstCat();
+  console.log(copyOfCats4)
